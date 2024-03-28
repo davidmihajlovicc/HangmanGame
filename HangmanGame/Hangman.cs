02b68b5d6d51fe3bb4e39e37b7fe61fa617e3471
David <David@DESKTOP-1B2P81V>
2024-03-28 14:05:04 +0100

    Add project files.

---
+++ HangmanGame/Hangman.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HangmanGame
+{
+    public class Hangman
+    {
+        private string word;
+        private StringBuilder answer;
+        private int lives;
+        public string Word { get { return word; } }
+        public int Lives { get { return lives; } }
+
+        public Hangman(string word) {
+            this.word = word;
+            lives = 5;
+            answer = new StringBuilder();
+            foreach (char letter in word) {
+                answer.Append("_");
+            }
+        }
+
+        public bool ContainsLetter(char letter) {
+            
+            return word.Contains(letter);
+
+        }
+
+        public void AddLetter(char letter) {
+            
+            for (int i = 0; i < word.Length; i++) {
+                if (letter == word[i]) {
+                    answer[i]=letter;
+                }
+            }
+            
+        }
+        public void ReduceLives() {
+            lives--;
+        }
+
+        public bool LetterUsedAlready(char letter) {
+            if (answer.ToString().Contains(letter)) {
+                return true;
+            }
+            return false;
+            
+        }
+
+        public bool WordGuessed() {
+            if (answer.ToString() == word) {
+                return true;
+            }
+            return false;
+        
+        }
+
+        public override string ToString()
+        {
+            return answer.ToString(); 
+        }
+
+
+    }
+}
---
+++ HangmanGame/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace HangmanGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random generator = new Random();
+
+            List<string> words = new List<string>() {
+                "account", "across", "amusement", "beautiful", "belief",
+                "brother", "cheese", "cloud", "damage", "daughter", "electric",
+                "experience", "feather",  "fixed", "government", "hanging", "healthy",
+                "instrument", "journey", "language", "living", "material",
+                "mountain", "normal", "observation", "parallel", "political",
+                "reaction", "representative", "servant", "sister", "tendency",
+                "thought", "weather", "winter", "Android"
+            };
+
+            Hangman hangman = new Hangman(words[generator.Next(words.Count)]);
+            Console.WriteLine("Guess the word!");
+            Console.WriteLine($"Word is {hangman.Word.Length} letters long. {hangman.Word}");
+            char letter;
+
+            do
+            {
+                if (hangman.WordGuessed())
+                {
+                    Console.WriteLine("You guessed the word!");
+                    break;
+                }
+
+                Console.WriteLine("Guess a letter:");
+                letter = char.ToLower(char.Parse(Console.ReadLine()));
+
+                if (hangman.ContainsLetter(letter))
+                {
+                    if (hangman.LetterUsedAlready(letter)) {
+                        Console.WriteLine("You already used this letter! Try another one.");
+                    }
+                    else {
+                        Console.WriteLine("You guessed a letter!");
+                        hangman.AddLetter(letter);
+                        Console.WriteLine(hangman.ToString());
+                    }   
+                }
+
+                else
+                {
+                    hangman.ReduceLives();
+                    Console.WriteLine("You guessed a wrong letter!");
+                    Console.WriteLine($"You have {hangman.Lives} lives left!");
+                }
+
+
+            } while (hangman.Lives > 0);
+
+        }
+    }
+}
